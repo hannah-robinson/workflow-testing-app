0dca74462ef9a87f9412dd135158ca09cfabc594
Hannah Robinson <hannah.robinson.git@gmail.com>
2023-10-15 16:43:26 +1300

    Remove unused components

--- extensions/test-banner/src/Checkout.jsx
+++ extensions/test-banner/src/Checkout.jsx
@@ -1,18 +1,10 @@
-import {
-  Banner,
-  useApi,
-  useTranslate,
-  reactExtension,
-} from '@shopify/ui-extensions-react/checkout'
+import { Banner, reactExtension } from '@shopify/ui-extensions-react/checkout'
 
 export default reactExtension('purchase.checkout.block.render', () => (
   <Extension />
 ))
 
 function Extension() {
-  const translate = useTranslate()
-  const { extension } = useApi()
-
   return (
     <Banner title='test-banner'>I am the released version of the app.</Banner>
   )
